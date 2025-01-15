020c83b2adea6d0edaa1940f023da0c62b8ecec4
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2025-01-15 19:09:32 -0300

    refactor: refactorinh the Creative

--- src/Aphrodite.Domain.Core/Entities/Creative.cs
+++ src/Aphrodite.Domain.Core/Entities/Creative.cs
@@ -4,9 +4,12 @@ namespace Aphrodite.Domain.Core.Entities;
 
 public class Creative : BaseEntity
 {
+    private readonly IList<Comment> _comments;
+    
     public Creative(
         string title, 
         string description, 
+        DateTime postingDate,
         Admin creator, 
         Customer customer, 
         Byte[] imageOrVideo)
@@ -14,20 +17,26 @@ public class Creative : BaseEntity
         Title = title;
         Description = description;
         IsApproved = false;
+        IsScheduled = false;
+        PostingDate = postingDate;
         CreatedAt = DateTime.Now;
         Creator = creator;
         Customer = customer;
         ImageOrVideo = imageOrVideo;
+        _comments = new List<Comment>();
     }
 
     public string Title { get; private set; }
     public string Description { get; private set; }
     public bool IsApproved { get; private set; }
+    public bool IsScheduled { get; private set; }
+    public DateTime PostingDate { get; private set; }
     public DateTime CreatedAt { get; private set; }
     public DateTime? ModifiedAt { get; private set; }
     public Admin Creator { get; private set; }
     public Customer Customer { get; private set; }
     public Byte[] ImageOrVideo { get; private set; }
+    public IReadOnlyCollection<Comment> Comments => _comments.ToArray();
 
     public void MarkAsApproved()
     {
@@ -44,5 +53,21 @@ public class Creative : BaseEntity
     {
         Description = newDescription;
         ModifiedAt = DateTime.Now;
+    }
+
+    public void UpdatePostingDate(DateTime newPostingDate)
+    {
+        PostingDate = newPostingDate;
+        ModifiedAt = DateTime.Now;
+    }
+
+    public void MarkAsScheduled()
+    {
+        IsScheduled = true;
+    }
+
+    public void AddComment(Comment comment)
+    {
+        _comments.Add(comment);
     }
 }
